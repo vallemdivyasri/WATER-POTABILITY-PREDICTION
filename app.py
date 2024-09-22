2f6b07acb448887f01d7646e75e2d926c7ded9a6
vallemdivyasri <138040618+vallemdivyasri@users.noreply.github.com>
2024-09-22 14:24:04 +0530

    app.py

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 import pickle
 import numpy as np
 from flask import Flask, request, render_template
+
 app = Flask(__name__)
 
 model = pickle.load(open('model.pkl', 'rb'))
@@ -8,18 +9,25 @@ model = pickle.load(open('model.pkl', 'rb'))
 
 @app.route('/')
 def home():
-    return render_template('index.html')
+    # Set prediction_text to None initially
+    return render_template('index.html', prediction_text=None)
 
 
 @app.route('/predict', methods=['POST'])
 def predict():
+    # Get form inputs
     float_features = [float(x) for x in request.form.values()]
     f = [np.array(float_features)]
+
+    # Predict water potability
     prediction = model.predict(f)
     if prediction == 0:
-        return render_template('index.html', prediction_text="Given Water sample is not Potable")
+        result = "Given Water sample is not Potable"
     else:
-        return render_template('index.html', prediction_text="Given Water sample is Potable")
+        result = "Given Water sample is Potable"
+
+    # Render the result in the template
+    return render_template('index.html', prediction_text=result)
 
 
 if __name__ == "__main__":
